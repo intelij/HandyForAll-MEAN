0b43befdc97916bf041b46004031c61275e57c56
Keep Simple <keepsimple17@gmail.com>
2018-05-04 04:09:35 +0800

    In
    Initial

---
+++ model/twilio.js
@@ -0,0 +1,26 @@
+var db = require('../controller/adaptor/mongodb.js');
+
+function createMessage(to, from, message, callback) {
+    db.GetOneDocument('settings', { alias: 'sms' }, {}, {}, function (err, document) {
+        if (err) {
+            callback(err, document);
+        } else {
+            var twilio = document.settings.twilio;
+            var client = require('twilio')(twilio.account_sid, twilio.authentication_token);
+            
+            if (!from) { from = twilio.default_phone_number }
+            client.messages.create({
+                to: to,
+                from: from,
+                body: message
+            }, function (err, message) {
+				console.log("err, message",err, message);
+                callback(err, message);
+            });
+        }
+    });
+}
+
+module.exports = {
+    "createMessage": createMessage
+};
