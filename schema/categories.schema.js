134826123c312e596dd2963408673a8379da8882
Keep Simple <keepsimple17@gmail.com>
2018-05-14 15:20:35 +0800

    feat(category): add some logic for third, forth. fifth category

--- schema/categories.schema.js
+++ schema/categories.schema.js
@@ -1,28 +1,29 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-var CATEGORIES_SCHEMA = {};
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const CATEGORIES_SCHEMA = {};
 CATEGORIES_SCHEMA.CATEGORIES = {
-    name: String,
-    slug: String,
-    position: Number,
-    commision: Number,
-    status: Number,
-    skills:[],
-    image: String,
-    img_name:String,
-    img_path:String,
-    icon:String,
-	activeicon:String,
-    icon_name:String,
-    icon_path:String,
-	marker:String,
-    seo: {
-        title: String,
-        keyword: String,
-        description: String
-    },
-	admincommision: Number,
-    parent: { type: Schema.ObjectId, ref: 'category' }
+  name: String,
+  slug: String,
+  position: Number,
+  commision: Number,
+  level: Number,
+  status: Number,
+  skills:[],
+  image: String,
+  img_name:String,
+  img_path:String,
+  icon:String,
+  activeicon:String,
+  icon_name:String,
+  icon_path:String,
+  marker:String,
+  seo: {
+    title: String,
+    keyword: String,
+    description: String
+  },
+  admincommision: Number,
+  parent: { type: Schema.ObjectId, ref: 'category' }
 };
 
 module.exports = CATEGORIES_SCHEMA;
